d43dc7d1a4d5b4d151a676e8c2bde3b9a245ba33
idanmalka <idan_malka@yahoo.com>
2016-07-17 19:34:34 +0300

    Add files via upload

---
+++ module 10/Personnel/Personnel/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personnel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+            FileStream stream = new FileStream("Names.txt", FileMode.OpenOrCreate, FileAccess.Read, FileShare.Read);
+            StreamReader reader = new StreamReader(stream);
+            while(!reader.EndOfStream) names.Add(reader.ReadLine());
+            foreach (var name in names)
+                Console.WriteLine(name);
+        }
+    }
+}
