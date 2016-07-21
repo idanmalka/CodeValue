69645a1c66e5986a1272abc19f94963aee6e18cb
idanmalka <idan_malka@yahoo.com>
2016-07-21 16:35:34 +0300

    Add files via upload

---
+++ advanced module 2.2/GenericApp/GenericApp/MultiDictionary.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GenericApp
+{
+    
+    public class MultiDictionary<K,V>  : IMultiDictionary<K, V>, IEnumerable<KeyValuePair<K, IEnumerable<V>>>  where V:struct
+    {
+        public ICollection<K> Keys => _dictionary.Keys;
+        public ICollection<V> Values { get; }
+        public int Count { get; private set; }
+        private Dictionary<K, LinkedList<V>> _dictionary;
+
+        public MultiDictionary()
+        {
+            Count = 0;
+            Values = new List<V>();
+            _dictionary = new Dictionary<K, LinkedList<V>>();
+        }
+
+        public void Add(K key, V value)
+        {
+            bool validKey = false;
+            var keyAttributes = key.GetType().GetCustomAttributes(typeof(KeyAttribute),false);
+
+            foreach (KeyAttribute att in keyAttributes)
+                validKey = true;
+            
+            if (!validKey) throw new Exception($"{key} is an invalid key");
+
+            if (!_dictionary.ContainsKey(key))
+                _dictionary.Add(key, new LinkedList<V>());
+
+            _dictionary[key].AddLast(value);
+            Values.Add(value);
+            ++Count;
+        }
+
+        public void CreateNewValue(K key)
+        {
+            var newItem = Activator.CreateInstance(typeof(V));
+            Add(key,(V)newItem);
+        }
+
+        public bool Remove(K key)
+        {
+            if (!ContainsKey(key)) throw new ArgumentOutOfRangeException();
+            Count -= _dictionary[key].Count;
+            foreach (var item in _dictionary[key])
+                Values.Remove(item);
+            return _dictionary.Remove(key);
+        }
+
+        public bool Remove(K key, V value)
+        {
+            if (_dictionary.ContainsKey(key))
+            {
+                Values.Remove(value);
+                --Count;
+                return _dictionary[key].Remove(value);
+
+            }
+            return false;
+        }
+
+        public void Clear()
+        {
+            _dictionary.Clear();
+            Count = 0;
+            Values.Clear();
+        }
+
+        public bool ContainsKey(K key)
+        {
+            return _dictionary.ContainsKey(key);
+        }
+        
+        public bool Contains(K key, V value)
+        {
+            if (_dictionary.ContainsKey(key))
+                return _dictionary[key].Contains(value);
+            return false;
+        }
+        
+
+        public IEnumerator<KeyValuePair<K, IEnumerable<V>>> GetEnumerator()
+        {
+            foreach (var pair in _dictionary)
+            {
+                yield return new KeyValuePair<K, IEnumerable<V>>(pair.Key, pair.Value);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ advanced module 2.2/GenericApp/GenericApp/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+
+namespace GenericApp
+{
+    [Key]
+    public struct Keys
+    {
+        public int mynumber { private set; get; }
+        private static Dictionary<int, Keys> _keys = new Dictionary<int, Keys>();
+        
+        public static implicit operator Keys(int key)
+        {
+            if (_keys.ContainsKey(key)) return _keys[key];
+            Keys newKey = new Keys();
+            newKey.mynumber = key;
+            _keys.Add(key, newKey);
+            return newKey;
+        }
+
+        public override string ToString()
+        {
+            return mynumber.ToString();
+        }
+    }// keys
+
+    public struct myString {
+
+        private string str;
+
+        private myString(string s)
+        {
+            str = s;
+        }
+
+        public static implicit operator myString(string s)
+        {
+            return new myString(s);           
+        }
+
+        public override string ToString()
+        {
+            return str;
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var dictionary = new MultiDictionary<Keys, myString>
+            {
+                {1, "one"},
+                {2, "two"},
+                {3, "three"},
+                {1, "ich"},
+                {2, "nee"},
+                {3, "sun"}
+            };
+
+            PrintDictionary(dictionary);
+
+            Console.WriteLine($"Does the dictionary contain a key \"1\"? \n{dictionary.ContainsKey(1)} to that brother");
+            Console.WriteLine($"Does the dictionary contain a key \"3\" and value \"sun\" ? \n{dictionary.Contains(3, "sun")} to that brother\n");
+            Console.WriteLine("Removing all one's");
+            try
+            {
+                dictionary.Remove(1);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("there was no key called \"1\"");
+            }
+
+            PrintDictionary(dictionary);
+            Console.WriteLine($"Now, does the dictionary contain a key \"1\"? \n{dictionary.ContainsKey(1)} to that brother\n");
+
+            Console.WriteLine("Removing just \"nee\":");
+            dictionary.Remove(2, "nee");
+            PrintDictionary(dictionary);
+
+            Console.WriteLine($"Now, does the dictionary contain a key \"2\" and value \"nee\"? \n{dictionary.Contains(2, "nee")} to that brother\n");
+
+            Console.WriteLine("Clearing Dictionary");
+            dictionary.Clear();
+            PrintDictionary(dictionary);
+        }
+
+
+        private static void PrintDictionary<T,V>(IMultiDictionary<T,V> dictionary) where V:struct
+        {
+            Console.WriteLine($"Dictionary Contains {dictionary.Count} values:");
+            Console.WriteLine($"Keys:");
+            foreach (var item in dictionary.Keys)
+                Console.Write($"{item} ");
+
+            Console.WriteLine();
+            Console.WriteLine($"Values:");
+            foreach (var item in dictionary.Values)
+                Console.Write($"{item} ");
+            Console.WriteLine("\n");
+        }
+    }
+}
