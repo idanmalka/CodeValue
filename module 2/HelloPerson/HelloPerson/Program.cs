5f76fcc4e44485014d3670d65dcdde0b243353d5
Moaid Hathot <moaid.hathot@outlook.com>
2016-07-23 21:09:13 +0300

    2.1

--- module 2/HelloPerson/HelloPerson/Program.cs
+++ module 2/HelloPerson/HelloPerson/Program.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace HelloPerson
 {
+    //No input validatoin.
+    //Print space at the first line - it is ok though.
     class Program
     {
         static void Main(string[] args)
@@ -14,12 +16,21 @@ namespace HelloPerson
             int num;
             Console.WriteLine("What is your name?");
             name = Console.ReadLine();
+
+            //Nice, C#'s 6 feature.
             Console.WriteLine($"Hello {name}");
             Console.WriteLine("enter a number from 1 to 10");
+
+            //Consider using int.TryParse
             num = int.Parse(Console.ReadLine());
             for (int i = 0; i < num; i++)
             {
-                for (int j = 0; j <= i; j++) Console.Write(" ");
+                //The convention in C# is use a seperate line for the body, and also to add curly bracets, even if it isnly for a one liner.
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(" ");
+                }
+
                 Console.WriteLine(name);
             }
         }
