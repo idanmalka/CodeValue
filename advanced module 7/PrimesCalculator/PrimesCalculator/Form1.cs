bfaabeb41848ca1ea9880a691ec29af502f33379
idanmalka <idan_malka@yahoo.com>
2016-08-13 22:43:58 +0300

    Add files via upload

---
+++ advanced module 7/PrimesCalculator/PrimesCalculator/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimesCalculator
+{
+    public partial class Form1 : Form
+    {
+        private readonly ManualResetEvent _stopResultsInputHandle = new ManualResetEvent(false);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CalcButton_Click(object sender, EventArgs e)
+        {
+            int first, last;
+            int.TryParse(firstNumberText.Text, out first);
+            int.TryParse(LastNumberText.Text, out last);
+            ThreadPool.QueueUserWorkItem((x) => ThreadCode(first,last));
+        }
+
+        private void ThreadCode(int first,int last)
+        {
+            var list = Enumerable.Range(first, last).Select(y => y).Where(IsPrime);
+            Invoke(new Action(() => Results.Items.Clear()));
+            UpdateResults(list);
+        }
+
+        private void UpdateResults(IEnumerable<int> list)
+        {
+            foreach (var num in list)
+            {
+                if (!CancelationCheck()) return;
+                Invoke(new Action(() => Results.Items.Add(num)));
+                Thread.Sleep(100);
+            }
+        }
+
+        private bool CancelationCheck()
+        {
+            if (!_stopResultsInputHandle.WaitOne(0)) return true;
+            Invoke(new Action(() => Results.Items.Add("Canceled")));
+            _stopResultsInputHandle.Reset();
+            return false;
+        }
+
+        private static bool IsPrime(int x)
+        {
+            if (x < 0) throw new ArgumentOutOfRangeException();
+            if (x == 1) return false;
+            for (int i = 2; i <= Math.Sqrt(x); i++)
+                if (x % i == 0) return false;
+            return true;
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            _stopResultsInputHandle.Set();
+        }
+    }
+}
---
+++ advanced module 7/Queues/Queues/LimitedQueue.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Queues
+{
+    class LimitedQueue<T> 
+    {
+        private readonly Queue<T> _queue;
+        private readonly SemaphoreSlim _writerSem,_readerSem;
+        public LimitedQueue(int size)
+        {
+            if (size<0)
+                throw new ArgumentOutOfRangeException();
+            _queue = new Queue<T>();
+            _writerSem = new SemaphoreSlim(size);
+            _readerSem = new SemaphoreSlim(0);
+        }
+        public void Enque(T item)
+        {
+            _writerSem.Wait();
+            lock (_queue)
+            {
+                _queue.Enqueue(item);
+                Console.WriteLine($"Number of items in the queue: {_queue.Count}");
+            }
+            _readerSem.Release();
+        }
+
+        public T Deque()
+        {
+            T dequedItem;
+            _readerSem.Wait();
+            lock (_queue)
+            {
+                dequedItem = _queue.Dequeue();
+                Console.WriteLine($"Number of items in the queue: {_queue.Count}");
+            }
+            _writerSem.Release();
+            return dequedItem;
+        }
+    }
+}
