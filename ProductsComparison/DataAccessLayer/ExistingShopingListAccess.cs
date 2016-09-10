08f998c7fb6918111f75a1d1bdf6a77a7d5027ce
idanmalka <idan_malka@yahoo.com>
2016-09-10 18:33:57 +0300

    Add files via upload

---
+++ ProductsComparison/DataAccessLayer/ExistingShopingListAccess.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DataAccessLayer
+{
+    public class ExistingShopingListAccess
+    {
+
+        public static void SaveCartToFile(List<ItemGeneralData> cart, string path)
+        {
+            var writer = new XmlSerializer(typeof(List<ItemGeneralData>));
+
+            var file = System.IO.File.Create(path);
+
+            writer.Serialize(file, cart);
+            file.Close();
+        }
+
+        public static List<ItemGeneralData> LoadCartFromFile(string path)
+        {
+            var reader = new System.Xml.Serialization.XmlSerializer(typeof(List<ItemGeneralData>));
+            var file = new System.IO.StreamReader(path);
+            List<ItemGeneralData> cart;
+            try
+            {
+                cart = (List<ItemGeneralData>) reader.Deserialize(file);
+            }
+            catch (InvalidOperationException e)
+            {
+                cart = new List<ItemGeneralData>
+                {
+                    new ItemGeneralData
+                    {
+                        ItemName = "Invalid File"
+                    }
+                };
+            }
+            file.Close();
+            return cart;
+        }
+    }
+}
